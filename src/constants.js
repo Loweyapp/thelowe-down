a287dc0fcabf31afbfcf293eef8afa07e2924746
Loweyapp <alexlowe1@gmail.com>
2026-05-18 19:51:59 +0100

    Add Nov/Dec 2025 historical data, bump to v1.3.8

--- src/constants.js
+++ src/constants.js
@@ -6,7 +6,7 @@ import {
   Banknote, PiggyBank, TrendingUp,
 } from 'lucide-react';
 
-export const VERSION = 'v1.3.6';
+export const VERSION = 'v1.3.8';
 
 export const C = {
   primary:    '#0ABFA3',
