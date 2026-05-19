6587e2846cbbbc30b8e96b47595713ddc3963218
Loweyapp <alexlowe1@gmail.com>
2026-05-19 15:02:34 +0100

    v1.3.20 global test mode banner

--- src/constants.js
+++ src/constants.js
@@ -6,7 +6,7 @@ import {
   Banknote, PiggyBank, TrendingUp,
 } from 'lucide-react';
 
-export const VERSION = 'v1.3.19';
+export const VERSION = 'v1.3.20';
 
 export const C = {
   primary:    '#0ABFA3',
